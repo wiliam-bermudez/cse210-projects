37196db04136a118de73040b4137ba41b9b40435
wiliam.bermudez <wbermdezsols@byupathway.edu>
2025-06-11 10:00:48 -0600

    Shapes practice

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square("blue", 5.0);
+        Rectangle rect1 = new Rectangle("White", 20.0, 5.0);
+        Circle circ1 = new Circle("Green", 20.0);
+
+
+        shapes.Add(square1);
+        shapes.Add(rect1);
+        shapes.Add(circ1);
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Shape color: {shape.GetColor()} \n Area: {area}")
+;        }
     }
 }
